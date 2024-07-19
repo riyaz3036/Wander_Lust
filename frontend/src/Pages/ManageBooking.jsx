b01d179005de40b13e4a4b0aee961ae9b3281939
Riyaz Ahmed <riyaz.mittu@gmail.com>
2024-07-19 22:54:20 +0530

    rectified booking date error

--- frontend/src/Pages/ManageBooking.jsx
+++ frontend/src/Pages/ManageBooking.jsx
@@ -79,7 +79,8 @@ const ManageBooking = () => {
                         <div className="booking__data">
                         <p><span>Booked Tour:</span>{booking.tour_id.title}</p>
                         <p><span>Guest Size:</span> {booking.guestSize}</p>
-                        <p><span>Booking Date:</span> {formatDate(booking.bookFor)}</p>
+                        <p><span>Tour Date</span> {formatDate(booking.tour_id.start_date)}</p>
+                        <p><span>Booking Name:</span> {booking.bookFor}</p>
                         <p><span>Price:</span>₹{booking.price}</p>
                         <p><span>Activities:</span></p>
                         {booking.signed_activities.length === 0 ? (
